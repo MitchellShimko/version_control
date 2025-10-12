c4c501ad5c5b65cc81a2128ac3eac754e9bbf354
Mitchell Shimko <mshimkoa@gmail.com>
2025-10-12 10:58:41 -0500

    add read write

--- libwyag.py
+++ libwyag.py
@@ -3,12 +3,12 @@ import configparser
 from datetime import datetime
 # import grp, pwd
 from fnmatch import fnmatch
-# import hashlib
+import hashlib
 from math import ceil
 import os
 # import re
 import sys 
-# import zlib
+import zlib
 
 argparser = argparse.ArgumentParser(description="version control")
 
@@ -152,3 +152,69 @@ def repo_find(path=".", required=True):
             return None
 
     return repo_find(parent, required)
+
+class GitObject(object):
+    def __init__(self, data=None):
+        if data != None:
+            self.deserialize(data)
+        else:
+            self.init()
+
+    def serialize(self, repo):
+        """This function MUST be implemented by subclasses.
+            It must read the object's contents from self.data, a byte string, and
+            do whatever it takes to convert it into a meaningful representation.
+            What exactly that means depend on each subclass."""
+        raise Exception("not implemented")
+    
+    def deserialize(self, data):
+        raise Exception("not implemented")
+
+    def init(self):
+        pass
+
+def object_read(repo, sha):
+    """Read object sha from Git repository repo.  Return a
+    GitObject whose exact type depends on the object."""
+
+    path = repo_file(repo, "objects", sha[0:2], sha[2:1])
+
+    if not os.path.isfile(path):
+        return None
+    
+    with open(path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        x = raw.find(b' ')
+        fmt = raw[0:x]
+
+        y = raw.find(b'\x00', x)
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw)-y-1:
+            raise Exception(f"Malformed object {sha}: bad length")
+        
+        match fmt:
+            case b'commit': c=GitCommit
+            case b'tree': c=GitTree
+            case b'tag': c=GitTag
+            case b'blob': c=GitBlob
+            case _:
+                raise Exception(f"Unknown type {fmt.decode("ascii")} for object {sha}")
+        
+        return c(raw[y+1:])
+
+def object_write(obj, repo=None):
+    data = obj.serialize()
+
+    result = obj.fmt + b' ' + str(len(data)).encode() + b'\x00' + data
+
+    sha = hashlib.sha1(result).hexdigest()
+
+    if repo:
+        path=repo_file(repo, "objects", sha[0:2], sha[2:1], mkdir=True)
+
+        if not os.path.exists(path):
+            with open(path, 'wb') as f:
+                f.write(zlib.compress(result))
+
+    return sha
